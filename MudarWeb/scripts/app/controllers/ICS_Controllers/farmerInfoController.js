386abbe0eaeae38904345ad5f2bcc5c798cfda7b
Bullardkarunkumar <karunmca2008@gmail.com>
2017-11-24 07:30:14 +0530

    farmer page added for upload files

---
+++ MudarWeb/scripts/app/controllers/ICS_Controllers/farmerInfoController.js
@@ -0,0 +1,48 @@
+﻿mudarApp.directive('ngFiles', ['$parse', function ($parse) {
+
+    function fn_link(scope, element, attrs) {
+        var onChange = $parse(attrs.ngFiles);
+        element.on('change', function (event) {
+            onChange(scope, { $files: event.target.files });
+        });
+    };
+
+    return {
+        link: fn_link
+    }
+}])
+
+    .controller('farmerInfoController', function ($http,$state, appSettings) {
+        debugger;
+        var self = this;        
+
+        var formdata = new FormData();
+        this.getTheFiles = function ($files) {
+            debugger;
+            angular.forEach($files, function (value, key) {
+                formdata.append(key, value);
+            });
+        };
+      
+        var _url =appSettings.apiServiceBaseUri + 'uploadfile/';
+        // NOW UPLOAD THE FILES.
+        this.uploadFiles = function () {
+            debugger;
+            var request = {
+                method: 'POST',
+                url: _url,
+                data: formdata,
+                headers: {
+                    'Content-Type': undefined
+                }              
+            };
+
+            // SEND THE FILES.
+            $http(request)
+                .success(function (d) {
+                    alert(d);
+                })
+                .error(function () {
+                });
+        }
+    });

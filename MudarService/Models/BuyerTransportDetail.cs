e1695f8c1cdfe755dedd6e11ae6bcac2bce44e3c
Bhagyashree <bhagyashreehegde15@gmail.com>
2017-11-29 08:17:27 +0530

    missing file

---
+++ MudarService/Models/BuyerTransportDetail.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MudarService.Models
+{
+    [Table("tblBuyerTransportDetails")]
+    public class BuyerTransportDetails : MudarBaseEntity
+    {
+        [Key]
+        public int BuyerTransportId { get; set; }
+        [ForeignKey("Buyer")]
+        public Guid BuyerId { get; set; }
+        public BuyerDetail Buyer { get; set; }
+        public int? ModeofTransport { get; set; }
+        public string SeaportName{get;set;}
+        public string AirportName { get; set; }
+        public string RoadDestination { get; set; }
+        public string RailStation { get; set; }
+    }
+}
